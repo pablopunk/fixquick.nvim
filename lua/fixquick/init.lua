d7203e43120fb6898eda6756424a57fd3f43a42c
Pablo P Varela <pablo@pablopunk.com>
2024-03-02 14:25:56 +0100

    seems to work

--- lua/fixquick/init.lua
+++ lua/fixquick/init.lua
@@ -7,26 +7,29 @@ local function make_buffer_modifiable()
 end
 
 --- @param args table
-local function on_quickfix_write(args)
-  local file = args.file
-  local file_contents = vim.fn.readfile(file)
-  local quickfix_entries = vim.fn.getqflist()
-  for index, entry in ipairs(quickfix_entries) do
-    local found = false
-    for _, line in ipairs(file_contents) do
-      if line:find(entry.text) then
-        found = true
-        break
+local function on_quickfix_write(file)
+  return function()
+    local file_contents = vim.fn.readfile(file)
+    local quickfix_entries = vim.fn.getqflist()
+    local new_qf_list = {}
+
+    for _, entry in ipairs(quickfix_entries) do
+      local found = false
+      for _, line in ipairs(file_contents) do
+        if line:find(entry.text) then
+          found = true
+          break
+        end
+      end
+      if found then
+        table.insert(new_qf_list, entry)
       end
     end
-    if not found then
-      -- remove entry from quickfix list
-      table.remove(quickfix_entries, index)
-    end
+
+    -- Replace the current quickfix list with the new one
+    vim.fn.setqflist({}, "r", { items = new_qf_list })
+    make_buffer_modifiable()
   end
-  -- create new quickfix list
-  vim.fn.setqflist({}, "r", { items = quickfix_entries })
-  make_buffer_modifiable()
 end
 
 local function on_quickfix_enter()
@@ -36,10 +39,10 @@ local function on_quickfix_enter()
 
     vim.cmd("write! " .. name)
     make_buffer_modifiable()
-    vim.api.nvim_create_autocmd("BufWriteCmd", {
+    vim.api.nvim_create_autocmd("BufWritePost", {
       group = augroup,
       buffer = bufnr,
-      callback = on_quickfix_write,
+      callback = on_quickfix_write(name),
     })
   end
 end
